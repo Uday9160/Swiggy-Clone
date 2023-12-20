02f953f95956e605b18bee5733baa321cba1a8ad
Vaibhav_Saroj <93854530+vaibhav1281@users.noreply.github.com>
2023-12-20 12:30:44 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,7 +2,7 @@ export const SWIGGY_API =
 "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D12.9351929%26lng%3D77.62448069999999%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
 
 export const SWIGGY_MAPI =
-"https://corsproxy.org/?https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=12.9351929&lng=77.62448069999999&carousel=true&third_party_vendor=1"
+"https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fmapi%2Frestaurants%2Flist%2Fv5%3Foffset%3D0%26is-seo-homepage-enabled%3Dtrue%26lat%3D12.9351929%26lng%3D77.62448069999999%26carousel%3Dtrue%26third_party_vendor%3D1"
 
 export const BEST_SLIDER = 
 "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/";
