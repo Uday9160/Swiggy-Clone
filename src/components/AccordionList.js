587bdbde2ecf2594ddbb6452572d08762159760f
Vaibhav_Saroj <93854530+vaibhav1281@users.noreply.github.com>
2023-12-21 13:47:56 +0530

    Update Menu
    Change the UI of Manu for Mobile, and added new feature in Food item description.

--- src/components/AccordionList.js
+++ src/components/AccordionList.js
@@ -1,6 +1,7 @@
 import { IMAGE_CDN } from "../utils/constant"
 import { useDispatch } from "react-redux";
 import { addItem } from "../redux/cartSlice";
+import { useState, useEffect } from "react";
 
 const AccordionList = ({items}) => {
     // console.log(items)
@@ -11,15 +12,59 @@ const AccordionList = ({items}) => {
         // Dispatch An Action
         dispatch(addItem(items))
     }
+
+    const[readMore, setReadMore] = useState(false);
+    const haldleDiscription = () => {
+        setReadMore(!readMore);
+    }
+
    
-    return(
+    return window.innerWidth <= 768 ? (
+        <div>
+            {
+                items.map((item) => (
+                    <div className="border-b-2 space-y-2 flex w-[398px] justify-between py-4">
+                        <div className="space-y-2 w-[230px]">
+                            <h1 className="font-bold text-base text-[#02060CBF]">{item.card.info.name}</h1>
+                            <p className="font-bold text-[#02060CEB] text-[16px]">₹ {item.card.info.price ? item.card.info.price / 100 : item.card.info.defaultPrice/100}</p>
+                            <div className="flex items-center space-x-1">
+                                {/* Start */}
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="#3d9b6d" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#3d9b6d" className="w-4 h-4">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                                    </svg>
+                                </span>
+                                {/* ratings */}
+                                <span className="text-[13px] font-bold text-[#116649] leading-4">
+                                    {item?.card?.info?.ratings?.aggregatedRating?.rating}
+                                </span>
+                                {/* ratind counts */}
+                                <span className="text-[#02060C99]">
+                                    ({item?.card?.info?.ratings?.aggregatedRating?.ratingCountV2})
+                                </span>
+                            </div>
+                            <p className="w-[230px] text-[#02060C99] text-sm font-semibold">
+                                {readMore ? item.card.info.description : `${item.card.info.description.slice(0,85)}...`}
+                                <span className="text-lg font-semibold text-black" onClick={haldleDiscription}>{readMore ? 'Less': 'More'}</span>
+                            </p>
+                        </div>
+                        <div className="w-[160px] h-[144px] flex flex-col items-center relative">
+                            <img className="w-[156px] h-[144px] rounded-lg absolute" src={IMAGE_CDN + item.card.info.imageId}/>
+                            <button className="w-24 h-9 border-2 rounded-lg text-green-500 font-bold box-border bg-white absolute -bottom-5">Add+</button>
+                            
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+    ) :(
             <div className="">
                 {
                     items.map((item) =>(
                         <div className="border-b-2 flex w-full py-7" key={item.card.info.id}>
                             <div className="w-10/12">
                                 <p className="font-bold">{item.card.info.name}</p>
-                                <p>₹ {item.card.info.price / 100}</p>
+                                <p>₹ {item.card.info.price ? item.card.info.price / 100 : item.card.info.defaultPrice/100}</p>
                                 <p>{item.card.info.description}</p>
                             </div>
                             <div className="w-2/12 relative">
